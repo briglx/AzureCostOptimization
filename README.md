d8f48f62ceebe89c49b0ffd8e00d04c260acb900
Brig Lamoreaux <bklamoreaux@gmail.com>
2019-08-02 16:44:26 -0700

    Add sources. Refactor Code

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 The `azure cost optimization` is an unofficial collection of scripts used to find useful information about an azure subscription.
 
 Azure provides metrics usage for their customers. The `azure cost optimzation` uses a script to fetch the data and use PowerBI M queries to parse the data info useful fields.
+
+# Resources
+- [https://stackoverflow.com/questions/57218402/finding-idle-virtual-machines-and-deallocating-them-using-azure-function]
+- [Supported Metrics](https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported)
+- [Get VM Metrics Powershell](https://docs.microsoft.com/en-us/powershell/module/az.monitor/get-azmetric?view=azps-2.5.0)
+- [Calucate properties](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/select-object?view=powershell-6)
+- [Storage Metrics example](https://docs.microsoft.com/en-us/rest/api/storageservices/storage-analytics-metrics-table-schema)
+- [Old Documentation to insert into tables with powershell](https://docs.microsoft.com/en-us/azure/storage/tables/table-storage-how-to-use-powershell#reference-cloudtable-property-of-a-specific-table)
+    - this indicates that PS AZ module doesn't have cloudtable features
+- [Blog post on tables and PS AZ module](https://paulomarquesc.github.io/working-with-azure-storage-tables-from-powershell/)
+- [Powershell example of tables ps2 az module](https://docs.microsoft.com/en-us/azure/storage/tables/table-storage-how-to-use-powershell)
+- [Power BI Integraton with Tables](https://blogs.endjin.com/2015/04/visualise-your-azure-table-storage-data-with-power-bi/)
+- [Example ps1 file for Add-AzTableRow](https://github.com/paulomarquesc/AzureRmStorageTable/blob/master/AzureRmStorageTableCoreHelper.psm1)
+- [Debugging Powershell in VSCode](https://code.visualstudio.com/docs/languages/powershell)
--- scripts/check-vm-utilization.ps1
+++ scripts/check-vm-utilization.ps1
@@ -1,55 +1,120 @@
-#Debug
-$PSVersionTable
-Get-InstalledModule -Name Az -AllVersions | Select-Object Name,Version
+<#
+.SYNOPSIS
+	check-vm-utilization.ps1 - PowerShell Script to fetch utilization data from Virtural Machines.
+.DESCRIPTION
+  	check-vm-utilization.psm1 - PowerShell Script to fetch utilization data from Virtural Machines.
+.NOTES
+	This script depends on Az.Accounts, Az.VirtualMachines, Az.Storage, and AzTable PowerShell modules	
+#>
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# want >= PS v6.2.2
-# want >= AZ 2.4.0
+#Requires -modules Az, AzTable
 
-try {
-    $null = Get-AzSubscription
-}
-catch {
-    Connect-AzAccount
-}
-
-# $storage_account = Get-AzStorageAccount  -name brigdrive -ResourceGroupName brigdrive
-
-$subscription = Get-AzSubscription
-
-$vms = Get-AzVM -Status
-
-# $vms[0] | Get-Member
-# $vms.PowerState
-Foreach ($vm in $vms)
+function Add-UtilizationRecord
 {
+	<#
+	.SYNOPSIS
+		Adds a Virtual Machine Utilization record a Storage Table.
+	.DESCRIPTION
+		Adds a Virtual Machine Utilization record a Storage Table.
+	.PARAMETER storageAccount
+        Storage Account where the utilization table lives
+    .PARAMETER tableName
+        Name of the utilization table to save records
+    .PARAMETER virtualMachineInstance
+        Virtual Machine Instance of the utilization metrics to fetch
+    .PARAMETER context
+        The current metadata used to authenticate Azure Resource Manager request
+	.EXAMPLE
+		# Getting latest utilization
+		$storageAccount = Get-AzStorageAccount ...
+		$tableName = "table01"
+        $vmInstance = (Get-AzVM -Status)[0] 
+        $context = Get-AzContext ...
+		Add-UtilizationRecord -StorageAccount $storageAccount -TableName $tableName -VirtualMachineInstance $vmInstance -Context $context
+	#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory=$true)]
+		$storageAccount,
+		
+		[Parameter(Mandatory=$true)]
+        [String]$tableName,
 
-    # name.value eq 'Disk Write Operations/Sec' or  name.value eq 'Percentage CPU' or  name.value eq 'Network In' or  name.value eq 'Network Out' or  name.value eq 'Disk Read Operations/Sec' or  name.value eq 'Disk Read Bytes' or  name.value eq 'Disk Write Bytes')
-    # and timeGrain eq duration'PT5M'
-    # and startTime eq 2017-10-26T05:28:34.919Z
-    # and endTime eq 2017-10-26T05:33:34.919Z
+		[Parameter(Mandatory=$true)]
+		$virtualMachineInstance,
+		
+		[Parameter(Mandatory=$true)]
+        $context
+    )
 
-    $time_now = Get-Date
-    $metric_result = Get-AzMetric -ResourceId $vm.Id -MetricName  @("Percentage CPU", "Disk Read Operations/Sec", "Disk Write Operations/Sec", "Network In", "Network Out", "Disk Write Bytes") -TimeGrain 00:05:00 -ResultType Data
+    #GLOBAL SETUP
+    # Get Storage and Table
+    # $storage_az_ctx = Get-AzContext
+    # $resource_group_name = "blxBilling"
+    # $storage_account_name = "blxbillingdiag"
+    # $storageAccount = Get-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storage_account_name -DefaultProfile $storage_az_ctx
+    $storageContext = $storageAccount.Context
 
-    $time_stamp = @{l="TimeStamp";e={$time_now.ToUniversalTime()}}
-    $month = @{l="Month";e={Get-Date $time_now -Format "yyyy-MM"}}
-    $subscription_id = @{l="Subscription";e={$subscription.Id}}
-    $id = @{l="Id";e={$vm.Id}}
-    $resource_group_name = @{l="ResourceGroupName";e={$vm.ResourceGroupName}}
+    # $table_name = 'MetricsDailyVm'
+    $storageTable  = Get-AzStorageTable –Name $tableName –Context $storageContext
+    $cloudTable = $storageTable.CloudTable
 
-    $resource_type = @{l="ResourceType";e={$vm.Type}}
-    $power_state = @{l="PowerState";e={$vm.PowerState}}
-    $local_name = @{l="Name";e={$_.Name.LocalizedValue}}
+    # GET CONTEXT
+    # $contexts = Get-AzContext -ListAvailable
+
+    # Foreach($context in $contexts){
+
+    #Set-AzContext -Context $context
+    $curSubscription = $context.Subscription
+
+    # Get vm Metrics
+    #$vms = Get-AzVM -Status 
+    #Foreach ($vm in $vms)
+    #{
+        
+    $timeNow = [DateTime]::UtcNow 
+
+    # Build Row Object
+    $row = new-object psobject
+    $row | add-member NoteProperty "Month" $timeNow.ToString("yyyy-MM")
+    $row | add-member NoteProperty "Subscription" $curSubscription.Id
+    $row | add-member NoteProperty "ResourceGroupName" $virtualMachineInstance.ResourceGroupName
+    $row | add-member NoteProperty "VmName" $virtualMachineInstance.Name
+    $row | add-member NoteProperty "Id" $virtualMachineInstance.Id
+    $row | add-member NoteProperty "ResourceType" $virtualMachineInstance.Type
+    $row | add-member NoteProperty "PowerState" $virtualMachineInstance.PowerState
+
+    # Metric Specific values
+    $localName = @{l="Name";e={$_.Name.LocalizedValue}}
     $average = @{l="Average";e={($_.Data.Average | Measure-Object -Average).Average}}
-    $data_points = @{l="DataPoints";e={$_.Data.Count}}
+    $dataPoints = @{l="DataPoints";e={$_.Data.Count}}
+
+    $metricResult = Get-AzMetric -ResourceId $virtualMachineInstance.Id -MetricName  @("Percentage CPU", "Disk Read Operations/Sec", "Disk Write Operations/Sec", "Network In", "Network Out", "Disk Write Bytes") -TimeGrain 00:05:00 -ResultType Data
+    $record = $metricResult | Select-Object -Property $localName,Unit,$average,$dataPoints
     
-    $metric_result | Select-Object -Property $time_stamp,$month,$subscription_id,$resource_group_name,$resource_type,$power_state,$id,$local_name,Unit,$average,$data_points
+    $pivots = $record | Select-Object -unique "Name" | ForEach-Object { $_.Name } | Sort-Object 
+    foreach ($pivot in $pivots)
+    {
+        $fieldName = $pivot -replace '\s|\/',''
+        $row | add-member NoteProperty $fieldName ($record | Where-Object {$_.Name -eq $pivot}).Average
+    }      
 
+    # Prep data for insert
+    $partitionKey = $timeNow | get-date -Format "yyyyMMddTHHmmZ"
+    $rowKey =  $curSubscription.Id + ";" + $virtualMachineInstance.ResourceGroupName + ";" + $virtualMachineInstance.Name
+
+    $hash = @{}
+    $row.psobject.properties | ForEach-Object { 
+        # Don't add null values
+        if($null -ne $_.Value ){
+            $hash[$_.Name] = $_.Value 
+        }
+    }
+
+    # Publish Row to Table Storage
+    Add-AzTableRow -table $cloudTable -partitionKey $partitionKey -rowKey ($rowKey) -property $hash
+
+    #}
 }
-
-#gps |Select -ExpandProperty Modules -ea SilentlyContinue |Group ModuleName |Sort Count |Select -Last 4
-# Resources
-# - https://stackoverflow.com/questions/57218402/finding-idle-virtual-machines-and-deallocating-them-using-azure-function
-# - Supported Metrics https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
-# - Get VM Metrics Powershell https://docs.microsoft.com/en-us/powershell/module/az.monitor/get-azmetric?view=azps-2.5.0
-# - Calucate properties https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/select-object?view=powershell-6
